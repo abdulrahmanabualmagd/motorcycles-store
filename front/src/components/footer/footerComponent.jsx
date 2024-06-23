8a66b41bc6d31b58657d3e70e1f677fb6de620fd
abdulrahmanabualmagd <abdulrahmanabualmagd@gmail.com>
2024-06-23 08:06:10 +0300

    Update App Layout

--- front/src/components/footer/footerComponent.jsx
+++ front/src/components/footer/footerComponent.jsx
@@ -1,7 +1,9 @@
-export default function FooterComponent(){
+import { Box, Typography } from "@mui/material";
+
+export default function FooterComponent() {
     return (
-        <div>
-            FooterComponent
-        </div>
-    )
+        <Box sx={{ p: 2, textAlign: "center", borderTop: "1px solid #e0e0e0" }}>
+            <Typography variant="body2">&copy; 2024 My App</Typography>
+        </Box>
+    );
 }
